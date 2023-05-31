ddfcdfed6b7a25c2c4b155253e65c863bea34279
Sungrinhan <sungrin92@gmail.com>
2023-06-01 04:58:03 +0900

    feat: make export index.ts file in pages folder

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as CartPage } from "./cart";
+export { default as ProductsPage } from "./products/index";
+export { default as AddProductPage } from "./products/add";
+export { default as ProductDetailPage } from "./products/detail";
