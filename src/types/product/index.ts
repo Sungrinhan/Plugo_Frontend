53077f473919b1b42774c6d6e3910c1031f3b90e
Sungrinhan <sungrin92@gmail.com>
2023-06-01 10:46:06 +0900

    feat: add types
    1. productDetailInfo

--- src/types/product/index.ts
+++ src/types/product/index.ts
@@ -12,7 +12,7 @@ export interface Product {
   images: [
     {
       url: string;
-    }
+    },
   ];
   productVariations: [
     {
@@ -27,11 +27,67 @@ export interface Product {
           location: {
             id: number;
           };
-        }
+        },
       ];
-    }
+    },
   ];
   releaseCountDown: boolean;
   numOrders: number;
   score: number;
+}
+
+export interface ProductDetailInfo {
+  id: number;
+  name: string;
+  productType: string;
+  releaseDate: string;
+  useStock: boolean;
+  preorderPolicy: string;
+  images: [
+    {
+      url: string;
+    },
+  ];
+  productVariations: [
+    {
+      id: number;
+      product: {
+        id: number;
+      };
+      price: number;
+      weight: number;
+      inventories: [
+        {
+          quantity: number;
+          location: {
+            id: number;
+          };
+        },
+      ];
+      details: [
+        {
+          id: number;
+          key: string;
+          value: string;
+          imageURL: string;
+        },
+      ];
+    },
+  ];
+  numOrders: number;
+  description: string;
+  estimatedDeliveryTimeInstock: string;
+  estimatedDeliveryTimePreorder: string;
+  productCode: string;
+  weight: number;
+  score: number;
+  vendorCategories: [
+    {
+      id: number;
+      name: string;
+      image: string;
+      sort: number;
+    },
+  ];
+  releaseCountDown: true;
 }
